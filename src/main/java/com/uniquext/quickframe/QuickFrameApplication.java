2c45472d689bf4885e06d23a7c897f480d6776e5
uniquext <penghaitao918@163.com>
2025-06-05 22:05:43 +0800

    Init Project

---
+++ src/main/java/com/uniquext/quickframe/QuickFrameApplication.java
@@ -0,0 +1,13 @@
+package com.uniquext.quickframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickFrameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniquext/quickframe/QuickFrameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquext.quickframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickFrameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
